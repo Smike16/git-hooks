31659eaebb07bf37c2122f31a99627b3ab8de295
Kostas Kryarov <kvkryarov@avito.ru>
2018-09-13 08:47:16 +0300

    Fixed problem in prepare-commit-msg hook

--- prepare-commit-msg.sh
+++ prepare-commit-msg.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
-# Adds branch name to commit message
+if [ -z "$BRANCHES_TO_SKIP" ]; then
+    BRANCHES_TO_SKIP=(master develop test)
+fi
 
 BRANCH_NAME=$(git symbolic-ref --short HEAD)
-TICKET_NAME=$(echo $BRANCH_NAME | grep -oE '\w+-\d+')
+TICKET_NAME=$(echo $BRANCH_NAME | sed -e 's:^\([^-]*-[^-]*\)-.*:\1:' -e \
+    'y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/')
+BRANCH_EXCLUDED=$(printf "%s\n" "${BRANCHES_TO_SKIP[@]}" | grep -c "^$BRANCH_NAME$")
+BRANCH_IN_COMMIT=$(grep -c "\[$BRANCH_NAME\]" $1)
 
-BRANCH_IN_COMMIT=$(grep -c "\[$TICKET_NAME\]" $1)
-
-if [ -n "$BRANCH_NAME" ] && ! [[ $BRANCH_IN_COMMIT -ge 1 ]]; then 
+if [ -n "$BRANCH_NAME" ] && ! [[ $BRANCH_EXCLUDED -eq 1 ]] && ! [[ $BRANCH_IN_COMMIT -ge 1 ]]; then 
     sed -i.bak -e "1s/^/$TICKET_NAME /" $1
 fi
